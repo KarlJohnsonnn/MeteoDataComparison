a22e9824903fae6a7b5e5de274ce2b9d88f25ca3
willi <willischimmel@gmx.de>
2018-11-28 12:00:43 +0100

    more user friendly, some comments

--- Compare_MDFs.py
+++ Compare_MDFs.py
@@ -12,8 +12,21 @@ from modules.Utility_Mod import *
    ##     ## ##     ##  ##  ##   ###         ##        ##    ##  ##     ## ##    ##  ##    ##  ##     ## ##     ##
    ##     ## ##     ## #### ##    ##         ##        ##     ##  #######   ######   ##     ## ##     ## ##     ##
 
+
+    The example call to the routine:    $  python Compare_MDFs.py > output.txt
+    
+    This will create a file "output.txt" containing all the prints to the console. 
+    The user has to set the path to both files manually to file1 and file2.
+
+
+
 ####################################################################################################################
 '''
+
+file1 = '/projekt2/remsens/data/LIMRAD94/leipzig/calibrated/2018/LV1/180808_050001_P01_ZEN.LV1.NC'
+file2 = '/projekt2/remsens/data/LIMRAD94/leipzig/calibrated/2018/LV1/180820_050000_P01_ZEN.LV1.NC'
+
+
 
 # Print Head
 if pts:
@@ -38,15 +51,17 @@ if pts:
 ######################################################################################################
 '''
 
-LR_lv1_a = nc2.LIMRAD94('/Users/willi/data/MeteoData/LIMRad94/calibrated/all/LV1/180808_050001_P01_ZEN.LV1.NC')
-LR_lv1_b = nc2.LIMRAD94('/Users/willi/data/MeteoData/LIMRad94/calibrated/all/LV1/180820_050000_P01_ZEN.LV1.NC')
+LR_lv1_a = nc2.LIMRAD94(file1)
+LR_lv1_b = nc2.LIMRAD94(file2)
 
+# create lists of constants and variables you want to compare
 constants_to_compare = ['AvgNum', 'NoiseFilt', 'SampDur', 'MaxVel', 'DoppRes']
 variables_to_compare = ['SeqIntTime', 'QualFlag', 'Status', 'TPow']
 
 print('')
 print('    COMPARE SAME CHIRP TABLES DIFFERENT DAY :: 08.08.18  vs. 20.08.18 \n')
 
+# compare the list of constants
 for item in constants_to_compare:
     eq = LR_lv1_a.dimensions[0][item] == LR_lv1_b.dimensions[0][item]
     print('')
@@ -56,6 +71,7 @@ for item in constants_to_compare:
         print('     --> a :: ', LR_lv1_a.dimensions[0][item])
         print('     --> b :: ', LR_lv1_b.dimensions[0][item])
 
+# compare the list of variables
 for item in variables_to_compare:
     eq = np.array_equal(LR_lv1_a.time_series_1D[0][item]['Val'],
                         LR_lv1_b.time_series_1D[0][item]['Val'])
